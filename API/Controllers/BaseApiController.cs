495d99c552cc8cd94066342d5aa30e3a4eb46f58
MuhammadSaadSaud <muhammadsaadsaud@gmail.com>
2022-03-04 00:00:50 +0100

    BaseApiController erstellt
    - enthält grundlegende Funktionen von ApiController

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Abgeleitet von ControllerBase und enthält
+    /// grundlegende Funktionen von ApiController
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
