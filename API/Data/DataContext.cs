0577a1dfd09d856f7d66125330d78b3f3799681c
MuhammadSaadSaud <muhammadsaadsaud@gmail.com>
2022-03-03 23:50:43 +0100

    DataContext erstellt
    - erstellt eine Verbindung zwischen Web-API und Datenbank

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    /// <summary>
+    /// [DataContext] erstellt eine Verbindung zwischen Web-API und Datenbank
+    /// [DbContext] primäre Klasse für die Interaktion mit der Datenbank
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="options">Es enthält alle erforderlichen
+        /// datenbankbezogenen Konfigurationsinformationen,
+        /// z. B. Datenbankanbieter (SQLite) </param>
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Ein DbSet repräsentiert die Collection aller Benutzer Entitäten
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+    }
+}
